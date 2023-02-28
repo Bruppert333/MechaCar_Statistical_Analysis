07c560f4ad3ab5ed71a7a297b73f32002ebff7b5
Bryce <bryceruppert@gmail.com>
2023-02-27 19:01:25 -0700

    Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
 library(dplyr)
 mechacar <- read.csv(file="MechaCar_mpg.csv")
-reg <- lm(vehicle_length~vehicle_weight~spoiler_angle~ground_clearance~AWD~mpg, data=mechacar)
+reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar)
 summary(reg)
