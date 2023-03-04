f17784cda0191cef4fea5fb9c2481d4b4c51e532
Bryce <bryceruppert@gmail.com>
2023-03-04 10:14:10 -0700

    Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,17 @@
+## Deliverable 1
+
 library(dplyr)
 mechacar <- read.csv(file="MechaCar_mpg.csv")
-reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar)
-summary(reg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechacar))
+
+## Deliverable 2
+
+suspension_coil <- read.csv ('Suspension_Coil.csv')
+
+total_summary <- summarize(suspension_coil, Mean=mean(suspension_coil$PSI),
+  Median=median(suspension_coil$PSI), Variance=var(suspension_coil$PSI), SD=sd(suspension_coil$PSI))
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
+
+## Deliverable 3
